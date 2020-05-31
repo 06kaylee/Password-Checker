fae31af990d7b3c4e3ef80b610f8129fac92cbb6
06kaylee <06.kaylee@gmail.com>
2020-05-31 11:55:51 -0700

    Initial commit

---
+++ checkmypass.py
@@ -0,0 +1,40 @@
+import requests
+import hashlib
+import sys
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def check_pwned_api(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    #send the first five characters to the api. Retruns list of results that matches those characters
+    res = request_api_data(first5_char)
+    #send the result to count method
+    return get_password_leaks_count(res, tail)
+
+
+def main(args):
+    #for each password provided in the command line
+    for password in args:
+        count = check_pwned_api(password)
+        if count:
+            print(f'{password} was found {count} times... you need to change your password')
+        else:
+            print(f'{password} was not found. Nice!')
+    return 'Completed'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
